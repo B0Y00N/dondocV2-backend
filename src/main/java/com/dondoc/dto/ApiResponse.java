df8549ca052504b612de1a0d271fcdc83ca957c4
유현 <cadry238@gmail.com>
2026-06-07 18:15:19 +0900

    feat(ApiResponse) : 제네릭으로 api 응답 요청 생성 (#8)

---
+++ src/main/java/com/dondoc/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+// 매번 API마다 이 구조를 새로 만들면 중복이 생겨셔, <T>로 제네릭하게 만들어서 data 부분만 API마다
+// 다르게 넣는다.
+
+package com.dondoc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse <T>{
+    private boolean success;
+    private T date;
+    private String message;
+
+}
